3fb6fcd33452f88710905bc3d9cca80656c7155d
shalousun <836575280@qq.com>
2023-06-11 12:12:20 +0800

    upgrade: smart-doc to 2.7.0

---
+++ src/main/java/com/power/doc/kubernetes/quarkus/enums/Sex.java
@@ -0,0 +1,14 @@
+package com.power.doc.kubernetes.quarkus.enums;
+
+
+public enum Sex {
+
+    /**
+     * male
+     */
+    MALE,
+    /**
+     * female
+     */
+    FEMALE
+}
--- src/main/java/com/power/doc/kubernetes/quarkus/rest/PathParamTestResource.java
+++ src/main/java/com/power/doc/kubernetes/quarkus/rest/PathParamTestResource.java
@@ -2,9 +2,11 @@ package com.power.doc.kubernetes.quarkus.rest;
 
 import com.power.doc.kubernetes.quarkus.model.MyResponse;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
 
 /**
  * JAX-RS path param example
@@ -22,6 +24,7 @@ public class PathParamTestResource {
      */
     @GET
     @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
     public MyResponse<String> SinglePathParameter(@PathParam("id") String id) {
         String msg = "id=" + id;
         return MyResponse.ok().setData(msg);
